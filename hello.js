6a85236904b40038ffeb1f6004d6e2924d88df35
Tushar Yadav <tusharyadav3529@gmail.com>
2025-08-21 17:41:35 +0530

    Added server.js and config.js

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
-console.log("Hi My name is tushar yadav");
+/* console.log("Hi My name is tushar yadav"); */
+
+/* function add(a,b){
+    return a+b;
+} */
+
+const math=require("./math.js")
+
+console.log(math(2,3)); 
